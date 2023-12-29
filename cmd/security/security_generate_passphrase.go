91f625726655485ab75e057698fc3cb1e23af614
Karn Wong <karn@karnwong.me>
2023-12-29 23:35:18 +0700

    add security: generate passphrase

---
+++ cmd/security/security_generate_passphrase.go
@@ -0,0 +1,43 @@
+package security
+
+import (
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/kahnwong/swissknife/cmd/utils"
+
+	"github.com/fatih/color"
+	"github.com/sethvargo/go-diceware/diceware"
+	"github.com/spf13/cobra"
+)
+
+func generatePassphrase() string {
+	// Generate 6 words using the diceware algorithm.
+	list, err := diceware.Generate(6)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	res := strings.Join(list, "-")
+
+	return res
+}
+
+var generatePassphraseCmd = &cobra.Command{
+	Use:   "generate-passphrase",
+	Short: "Generate passphrase",
+	Long:  `Generate passphrase. Result is copied to clipboard.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		// main
+		color.Green("Security: generate passphrase")
+
+		passphrase := generatePassphrase()
+		utils.CopyToClipboard(passphrase)
+		fmt.Printf("%s\n", passphrase)
+	},
+}
+
+func init() {
+	Cmd.AddCommand(generatePassphraseCmd)
+}
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.20
 
 require (
 	github.com/fatih/color v1.15.0
+	github.com/sethvargo/go-diceware v0.3.0
 	github.com/sethvargo/go-password v0.2.0
 	github.com/spf13/cobra v1.7.0
 	golang.design/x/clipboard v0.7.0
