62d0d8fe5e726c8dfd40aa62679e2cab6864de20
Karn Wong <karn@karnwong.me>
2024-06-08 19:31:43 +0700

    add test for generate ssh-key

---
+++ cmd/generate/generate_ssh_key_test.go
@@ -0,0 +1,23 @@
+package generate
+
+import (
+	"strings"
+	"testing"
+)
+
+func Test_generateSSHKey(t *testing.T) {
+	publicKey, privateKey, err := generateSSHKeyEDSA()
+	if err != nil {
+		t.Errorf("generateSSHKeyEDSA() error = %v", err)
+	} else {
+		// assert public key
+		if !strings.HasPrefix(publicKey, "ssh-ed25519") {
+			t.Errorf("generateSSHKeyEDSA() publicKey is not a public key")
+		}
+
+		// assert private key
+		if !strings.HasPrefix(privateKey, "-----BEGIN OPENSSH PRIVATE KEY-----") {
+			t.Errorf("generateSSHKeyEDSA() privateKey is not a private key")
+		}
+	}
+}
