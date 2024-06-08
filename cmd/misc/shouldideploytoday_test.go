1feba31f6b7b4debd9f8c42f7815791b1d64a07b
Karn Wong <karn@karnwong.me>
2024-06-08 20:29:31 +0700

    add test for shouldideploytoday

---
+++ cmd/misc/shouldideploytoday_test.go
@@ -0,0 +1,16 @@
+package misc
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestShouldIDeployToday(t *testing.T) {
+	response, err := ShouldIDeployToday()
+	if err != nil {
+		t.Errorf("ShouldIDeployToday() error = %v", err)
+
+	} else if reflect.TypeOf(response.Message).Kind() != reflect.String {
+		t.Errorf("ShouldIDeployToday() does not return a string")
+	}
+}
