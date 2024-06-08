695ef3b052878f4f8d947db84001c774c81df48e
Karn Wong <karn@karnwong.me>
2024-06-08 19:55:50 +0700

    add test for get iface

---
+++ cmd/get/get_iface_test.go
@@ -0,0 +1,19 @@
+package get
+
+import (
+	"strings"
+	"testing"
+)
+
+func Test_getIface(t *testing.T) {
+	iface, err := getIface()
+	if err != nil {
+		t.Errorf("getIface() error = %v", err)
+	} else {
+		isEn := strings.HasPrefix(iface, "en")
+		isUtun := strings.HasPrefix(iface, "utun")
+		if !(isEn || isUtun) {
+			t.Errorf("getIface() does not return expected interface: %s", iface)
+		}
+	}
+}
