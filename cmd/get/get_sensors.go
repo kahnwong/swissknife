1ec2696b4724f3f1b6510682f1f37423bd2a1bd7
Karn Wong <karn@karnwong.me>
2024-08-07 13:06:09 +0700

    feat: add sensors

---
+++ cmd/get/get_sensors.go
@@ -0,0 +1,36 @@
+package get
+
+import (
+	"fmt"
+
+	"github.com/kahnwong/swissknife/color"
+	"github.com/rs/zerolog/log"
+	"github.com/shirou/gopsutil/v4/sensors"
+	"github.com/spf13/cobra"
+)
+
+var SensorsCmd = &cobra.Command{
+	Use:   "sensors",
+	Short: "Get sensors info",
+	Run: func(cmd *cobra.Command, args []string) {
+		sensors, err := sensors.SensorsTemperatures()
+		if err != nil {
+			log.Fatal().Err(err).Msg("Failed to get sensors info")
+		}
+
+		var temperature float64
+		for _, sensor := range sensors {
+			if sensor.SensorKey == "PMU" {
+				if temperature < sensor.Temperature {
+					temperature = sensor.Temperature
+				}
+			}
+		}
+
+		fmt.Printf("%s: %.2f\n", color.Green("Temperature"), temperature)
+	},
+}
+
+func init() {
+	Cmd.AddCommand(SensorsCmd)
+}
