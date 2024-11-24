4e3f26c9242fcdf378bbe95b47c0afc4686c92d0
Karn Wong <karn@karnwong.me>
2024-11-24 16:36:33 +0700

    feat(listVolumes): add colors to table output

--- cmd/list/list_volumes.go
+++ cmd/list/list_volumes.go
@@ -2,38 +2,77 @@ package list
 
 import (
 	"fmt"
+	"os"
+	"strings"
+
+	"github.com/jedib0t/go-pretty/v6/table"
 
 	human "github.com/dustin/go-humanize"
+	"github.com/kahnwong/swissknife/color"
 	"github.com/shirou/gopsutil/v4/disk"
 	"github.com/spf13/cobra"
 )
 
-func listVolumes() string {
+func listVolumes() {
 	// ref: <https://stackoverflow.com/a/64141403>
-	formatter := "%-14s %7s %7s %7s %4s %s\n"
-	fmt.Printf(formatter, "Filesystem", "Size", "Used", "Avail", "Use%", "Mounted on")
+	// layout inspired by `duf`
 
+	// init table
+	t := table.NewWriter()
+	t.SetOutputMirror(os.Stdout)
+	t.AppendHeader(table.Row{"Mounted on", "Size", "Used", "Avail", "Use%", "Type", "Filesystem"})
+
+	// get volumes info
 	partitions, _ := disk.Partitions(false)
 	for _, partition := range partitions {
-		device := partition.Mountpoint
-		stats, _ := disk.Usage(device)
+		if partition.Fstype != "squashfs" && !strings.Contains(partition.Mountpoint, "snap") { // for example: snap partitions on ubuntu
+			device := partition.Mountpoint
+			stats, _ := disk.Usage(device)
 
-		if stats.Total == 0 {
-			continue
+			if stats.Total == 0 {
+				continue
+			}
+
+			// disk available
+			diskAvail := stats.Free
+			diskAvailStr := ""
+			if diskAvail < 50*1024*1024*1024 { // if free space less than 50 GB
+				diskAvailStr = color.Red(human.Bytes(diskAvail))
+			} else if diskAvail < 100*1024*1024*1024 { // if free space less than 100 GB
+				diskAvailStr = color.Yellow(human.Bytes(diskAvail))
+			} else {
+				diskAvailStr = color.Green(human.Bytes(diskAvail))
+			}
+
+			// disk use percent
+			percent := fmt.Sprintf("%2.f%%", stats.UsedPercent)
+			percentRaw := stats.UsedPercent
+			percentStr := ""
+			if percentRaw > 80 {
+				percentStr = color.Red(percent)
+			} else if percentRaw > 70 {
+				percentStr = color.Yellow(percent)
+			} else {
+				percentStr = color.Green(percent)
+			}
+
+			// append info to table
+			t.AppendRows([]table.Row{
+				{
+					color.Blue(partition.Mountpoint),
+					human.Bytes(stats.Total),
+					human.Bytes(stats.Used),
+					diskAvailStr,
+					percentStr,
+					color.Magenta(partition.Fstype),
+					color.Magenta(stats.Fstype),
+				},
+			})
 		}
-
-		percent := fmt.Sprintf("%2.f%%", stats.UsedPercent)
-
-		fmt.Printf(formatter,
-			stats.Fstype,
-			human.Bytes(stats.Total),
-			human.Bytes(stats.Used),
-			human.Bytes(stats.Free),
-			percent,
-			partition.Mountpoint,
-		)
 	}
-	return "foo"
+
+	// render
+	t.Render()
 }
 
 var listVolumesCmd = &cobra.Command{
--- color/constants.go
+++ color/constants.go
@@ -3,7 +3,9 @@ package color
 import "github.com/fatih/color"
 
 var (
-	Blue  = color.New(color.FgBlue).SprintFunc()
-	Green = color.New(color.FgHiGreen).SprintFunc()
-	Red   = color.New(color.FgRed).SprintFunc()
+	Blue    = color.New(color.FgBlue).SprintFunc()
+	Green   = color.New(color.FgHiGreen).SprintFunc()
+	Magenta = color.New(color.FgHiMagenta).SprintFunc()
+	Red     = color.New(color.FgRed).SprintFunc()
+	Yellow  = color.New(color.FgYellow).SprintFunc()
 )
