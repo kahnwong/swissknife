26de7b76bf9d3c0b48922a824926bd2a342c0d85
Karn Wong <karn@karnwong.me>
2025-06-26 14:47:22 +0700

    test

--- cmd/root.go
+++ cmd/root.go
@@ -2,9 +2,9 @@ package cmd
 
 import (
 	"fmt"
-	"os"
-
 	"github.com/anatol/smart.go"
+	"github.com/jaypipes/ghw"
+	"os"
 
 	"github.com/kahnwong/swissknife/cmd/generate"
 	"github.com/kahnwong/swissknife/cmd/get"
@@ -30,25 +30,51 @@ func Execute() {
 }
 
 func init() {
+	block, err := ghw.Block()
+	if err != nil {
+		panic(err)
+	}
+	for _, disk := range block.Disks {
+		dev, err := smart.Open("/dev/" + disk.Name)
+		if err != nil {
+			// some devices (like dmcrypt) do not support SMART interface
+			fmt.Println(err)
+			continue
+		}
+		defer dev.Close()
 
+		switch sm := dev.(type) {
+		case *smart.SataDevice:
+			data, _ := sm.ReadSMARTData()
+			fmt.Println(data)
+			//attr, ok := data.Attrs[194]; ok { // attr.Name == "Temperature_Celsius"
+			//temp, min, max, overtempCounter, err := attr.ParseAsTemperature()
+			//// min/max/counter are optional
+		//}
+		case *smart.ScsiDevice:
+			_, _ = sm.Capacity()
+		case *smart.NVMeDevice:
+			_, _ = sm.ReadSMART()
+		}
+	}
 	// skip the error handling for more compact API example
-	dev, _ := smart.OpenNVMe("/dev/disk3s1s1")
-	c, nss, _ := dev.Identify()
-	fmt.Println("Model number: ", c.ModelNumber())
-	fmt.Println("Serial number: ", c.SerialNumber())
-	fmt.Println("Size: ", c.Tnvmcap.Val[0])
-
-	// namespace #1
-	ns := nss[0]
-	fmt.Println("Namespace 1 utilization: ", ns.Nuse*ns.LbaSize())
-
-	sm, _ := dev.ReadSMART()
-	fmt.Println("Temperature: ", sm.Temperature, "K")
-	// PowerOnHours is reported as 128-bit value and represented by this library as an array of uint64
-	fmt.Println("Power-on hours: ", sm.PowerOnHours.Val[0])
-	fmt.Println("Power cycles: ", sm.PowerCycles.Val[0])
-
-	fmt.Println(sm.PercentUsed)
+	//dev, _ := smart.OpenNVMe("/dev/disk3s1s1")
+	//c, nss, _ := dev.Identify()
+	//fmt.Println("Model number: ", c.ModelNumber())
+	//fmt.Println("Serial number: ", c.SerialNumber())
+	//fmt.Println("Size: ", c.Tnvmcap.Val[0])
+	//
+	//// namespace #1
+	//ns := nss[0]
+	//fmt.Println("Namespace 1 utilization: ", ns.Nuse*ns.LbaSize())
+	//
+	//sm, _ := dev.ReadSMART()
+	//fmt.Println("Temperature: ", sm.Temperature, "K")
+	//// PowerOnHours is reported as 128-bit value and represented by this library as an array of uint64
+	//fmt.Println("Power-on hours: ", sm.PowerOnHours.Val[0])
+	//fmt.Println("Power cycles: ", sm.PowerCycles.Val[0])
+	//
+	//fmt.Println(sm.PercentUsed)
 
 	rootCmd.AddCommand(get.Cmd)
 	rootCmd.AddCommand(generate.Cmd)
--- go.mod
+++ go.mod
@@ -26,6 +26,7 @@ require (
 )
 
 require (
+	github.com/StackExchange/wmi v1.2.1 // indirect
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
 	github.com/charmbracelet/colorprofile v0.2.3-0.20250311203215-f60798e515dc // indirect
 	github.com/charmbracelet/harmonica v0.2.0 // indirect
@@ -37,15 +38,19 @@ require (
 	github.com/go-ole/go-ole v1.2.6 // indirect
 	github.com/google/gopacket v1.1.19 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/jaypipes/ghw v0.17.0 // indirect
+	github.com/jaypipes/pcidb v1.0.1 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/lufia/plan9stats v0.0.0-20211012122336-39d0f177ccd0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-localereader v0.0.1 // indirect
 	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/muesli/ansi v0.0.0-20230316100256-276c6243b2f6 // indirect
 	github.com/muesli/cancelreader v0.2.2 // indirect
 	github.com/muesli/termenv v0.16.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/power-devops/perfstat v0.0.0-20210106213030-5aafc221ea8c // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/spf13/pflag v1.0.6 // indirect
@@ -57,5 +62,6 @@ require (
 	golang.org/x/sync v0.15.0 // indirect
 	golang.org/x/sys v0.33.0 // indirect
 	golang.org/x/text v0.26.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 	howett.net/plist v1.0.0 // indirect
 )
