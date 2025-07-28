11a5c7e8565dfa1ef9de3ca224bd7fd3b04c99dd
Karn Wong <karn@karnwong.me>
2025-07-28 10:44:40 +0700

    refactor(shouldideploytoday): move to internal

--- cmd/shouldideploytoday.go
+++ cmd/shouldideploytoday.go
@@ -1,54 +1,15 @@
 package cmd
 
 import (
-	"fmt"
-	"time"
-
-	"context"
-
-	"github.com/carlmjohnson/requests"
-	"github.com/kahnwong/swissknife/color"
-	"github.com/rs/zerolog/log"
+	"github.com/kahnwong/swissknife/internal/shouldideploytoday"
 	"github.com/spf13/cobra"
 )
-
-type ShouldIDeploy struct {
-	Timezone      string    `json:"timezone"`
-	Date          time.Time `json:"date"`
-	ShouldIDeploy bool      `json:"shouldideploy"`
-	Message       string    `json:"message"`
-}
-
-func ShouldIDeployToday() ShouldIDeploy {
-	url := "https://shouldideploy.today"
-
-	var response ShouldIDeploy
-	err := requests.
-		URL(url).
-		Path("api").
-		Param("tz", "UTC").
-		Param("date", time.Now().UTC().Format("2006-01-02T15:04:05.000Z")).
-		ToJSON(&response).
-		Fetch(context.Background())
-
-	if err != nil {
-		log.Fatal().Msg("Error calling ShouldIDeploy API")
-	}
-
-	return response
-}
 
 var ShouldIDeployTodayCmd = &cobra.Command{
 	Use:   "shouldideploytoday",
 	Short: "Should I deploy today?",
 	Run: func(cmd *cobra.Command, args []string) {
-		response := ShouldIDeployToday()
-
-		if response.ShouldIDeploy {
-			fmt.Printf("%s\n", color.Green(response.Message))
-		} else if !response.ShouldIDeploy {
-			fmt.Printf("%s\n", color.Red(response.Message))
-		}
+		shouldideploytoday.ShouldIDeployToday()
 	},
 }
 
---
+++ internal/shouldideploytoday/shouldideploytoday.go
@@ -0,0 +1,47 @@
+package shouldideploytoday
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/carlmjohnson/requests"
+	"github.com/kahnwong/swissknife/configs"
+	"github.com/rs/zerolog/log"
+)
+
+type ShouldIDeploy struct {
+	Timezone      string    `json:"timezone"`
+	Date          time.Time `json:"date"`
+	ShouldIDeploy bool      `json:"shouldideploy"`
+	Message       string    `json:"message"`
+}
+
+func getResponse() ShouldIDeploy {
+	url := "https://shouldideploy.today"
+
+	var response ShouldIDeploy
+	err := requests.
+		URL(url).
+		Path("api").
+		Param("tz", "UTC").
+		Param("date", time.Now().UTC().Format("2006-01-02T15:04:05.000Z")).
+		ToJSON(&response).
+		Fetch(context.Background())
+
+	if err != nil {
+		log.Fatal().Msg("Error calling ShouldIDeploy API")
+	}
+
+	return response
+}
+
+func ShouldIDeployToday() {
+	response := getResponse()
+
+	if response.ShouldIDeploy {
+		fmt.Printf("%s\n", configs.Colors.Green(response.Message))
+	} else if !response.ShouldIDeploy {
+		fmt.Printf("%s\n", configs.Colors.Red(response.Message))
+	}
+}
