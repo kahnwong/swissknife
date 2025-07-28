57f6d896ef188bb6eeb4f60ade81cfbc818b64b5
Karn Wong <karn@karnwong.me>
2025-07-28 11:05:51 +0700

    refactor(generateQrcode): move to internal

---
+++ internal/generate/qrcode.go
@@ -0,0 +1,65 @@
+package generate
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/kahnwong/swissknife/internal/utils"
+	"github.com/rs/zerolog/log"
+	qrcode "github.com/skip2/go-qrcode"
+)
+
+func setURL(args []string) string {
+	var url string
+	if len(args) == 0 {
+		urlFromClipboard := utils.ReadFromClipboard()
+		if urlFromClipboard != "" {
+			if strings.HasPrefix(urlFromClipboard, "https://") {
+				url = urlFromClipboard
+			}
+		}
+	}
+	if url == "" {
+		if len(args) == 0 {
+			fmt.Println("Please specify URL")
+			os.Exit(1)
+		} else if len(args) == 1 {
+			url = args[0]
+		}
+	}
+
+	return url
+}
+
+func generateQRCode(url string) ([]byte, string) {
+	// init
+	var q *qrcode.QRCode
+	q, err := qrcode.New(url, qrcode.Medium)
+	if err != nil {
+		log.Fatal().Msg("Failed to initialize QRCode object")
+	}
+
+	// generate png
+	png, err := q.PNG(1024)
+	if err != nil {
+		log.Fatal().Msg("Failed to generate QRCode PNG")
+	}
+
+	// for stdout
+	//stdout := q.ToString(false)
+	stdout := q.ToSmallString(false)
+
+	return png, stdout
+}
+
+func QRCode(args []string) {
+	// set URL
+	url := setURL(args)
+	fmt.Println(url)
+
+	// main
+	png, stdout := generateQRCode(url)
+	utils.WriteToClipboardImage(png)
+	fmt.Println(stdout)
+}
