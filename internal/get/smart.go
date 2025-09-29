657679e782143105f385f2478bb46d8fbd13dd4c
Karn Wong <karn@karnwong.me>
2025-09-29 20:16:19 +0700

    feat(getSmart): add sata

--- internal/get/smart.go
+++ internal/get/smart.go
@@ -10,28 +10,48 @@ import (
 	"github.com/shirou/gopsutil/v4/disk"
 )
 
+func nvmeSmart(device string) error {
+	var err error
+	dev, err := smart.OpenNVMe(device)
+	if err != nil {
+		return err
+	} else {
+		sm, err := dev.ReadSMART()
+		if err != nil {
+			return err
+		}
+		fmt.Printf("%s: %v\n", color.Green("Percent Used"), sm.PercentUsed)
+
+		return nil
+	}
+}
+
+func sataSmart(device string) error {
+	var err error
+	dev, err := smart.OpenSata(device)
+	if err != nil {
+		return err
+	} else {
+		sm, err := dev.ReadSMARTData()
+		if err != nil {
+			return err
+		}
+		fmt.Printf("%s: %v\n", color.Green("Media Wearout Indicator"), sm.Attrs[230].Current)
+
+		return nil
+	}
+}
+
 func Smart() {
 	if runtime.GOOS == "linux" {
 		device := getRootDiskVolume()
 
-		isNvme := true
-		dev, err := smart.OpenNVMe(device)
+		err := nvmeSmart(device)
 		if err != nil {
-			log.Fatal().Err(err).Msgf("Failed to open %s device. You probaby have to run as sudo", device)
-		}
-		defer func(dev smart.Device) {
-			err = dev.Close()
+			err = sataSmart(device)
 			if err != nil {
-				log.Fatal().Err(err).Msgf("Failed to close %s device", device)
+				log.Error().Err(err).Msg("Unrecognized device type. It's not NVME or SATA, or you didn't run as sudo")
 			}
-		}(dev)
-
-		if isNvme {
-			sm, err := dev.ReadSMART()
-			if err != nil {
-				log.Fatal().Msg("Failed to read SMART info")
-			}
-			fmt.Printf("%s: %v\n", color.Green("Percent Used"), sm.PercentUsed)
 		}
 	} else {
 		log.Error().Msgf("%s is not supported\n", runtime.GOOS)
