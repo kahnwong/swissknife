bc4644f139043f7b5b33b250e774b46c72eee229
Karn Wong <karn@karnwong.me>
2026-01-09 10:36:48 +0700

    refactor: consolidate args inference

---
+++ internal/utils/args.go
@@ -0,0 +1,42 @@
+package utils
+
+import (
+	"fmt"
+	"net"
+	"os"
+	"strings"
+)
+
+func setValueFromArgsOrClipboard(args []string, validator func(string) bool, errorMsg string, allowEmpty bool) string {
+	var value string
+	if len(args) == 0 {
+		clipboardValue := ReadFromClipboard()
+		if clipboardValue != "" && validator(clipboardValue) {
+			value = strings.TrimSpace(clipboardValue)
+		}
+	}
+	if value == "" {
+		if len(args) == 0 {
+			if allowEmpty {
+				return ""
+			}
+			fmt.Println(errorMsg)
+			os.Exit(1)
+		} else if len(args) == 1 {
+			value = args[0]
+		}
+	}
+	return value
+}
+
+func SetURL(args []string) string {
+	return setValueFromArgsOrClipboard(args,
+		func(s string) bool { return strings.HasPrefix(s, "https://") },
+		"Please specify URL", false)
+}
+
+func SetIP(args []string) string {
+	return setValueFromArgsOrClipboard(args,
+		func(s string) bool { return net.ParseIP(strings.TrimSpace(s)) != nil },
+		"", true)
+}
